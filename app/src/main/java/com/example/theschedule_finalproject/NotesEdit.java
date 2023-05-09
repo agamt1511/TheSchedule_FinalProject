ab8453efa58fbde7a3ccaa1230a478c312ea2cae
agamt1511 <at1511@bs.amalnet.k12.il>
2023-05-09 19:27:32 +0300

    Starting Notes

--- app/src/main/java/com/example/theschedule_finalproject/NotesEdit.java
+++ app/src/main/java/com/example/theschedule_finalproject/NotesEdit.java
@@ -1,5 +1,6 @@
 package com.example.theschedule_finalproject;
 
+import static com.example.theschedule_finalproject.FBref.authRef;
 import static com.example.theschedule_finalproject.FBref.currentUser;
 import static com.example.theschedule_finalproject.FBref.notesRef;
 import static com.example.theschedule_finalproject.FBref.storageRef;
@@ -11,6 +12,7 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.net.ConnectivityManager;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.EditText;
@@ -18,13 +20,17 @@ import android.widget.EditText;
 import com.example.theschedule_finalproject.Models.Note;
 
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 
 public class NotesEdit extends AppCompatActivity {
+    //הכרזה על רכיבי תצוגה, משתנים וכדומה
     BroadcastReceiver broadcastReceiver;
     EditText title_etNE, txt_etNE;
     CheckBox thumbtack_cbNE;
     Note note;
+    String dateTime_created, thumbtackStatus;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,52 +41,76 @@ public class NotesEdit extends AppCompatActivity {
         broadcastReceiver = new NetworkConnectionReceiver();
         registerReceiver(broadcastReceiver,new IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION));
 
-
+        //התאמה בין רכיב תצוגה למשתנה
         title_etNE = (EditText) findViewById(R.id.title_etNE);
         txt_etNE = (EditText) findViewById(R.id.txt_etNE);
         thumbtack_cbNE = (CheckBox) findViewById(R.id.thumbtack_cbNE);
+
+        //אתחול והגדרת תוכן למשתנים ועצמים
         note = new Note();
+        currentUser = authRef.getCurrentUser();
     }
 
     public void saveNote(View view) {
+        //קליטת נתונים בסיסיים
+        String title = title_etNE.getText().toString();
+        String note_txt = txt_etNE.getText().toString();
+        thumbtackStatus = "noThumbtack";
 
-        if (dataVerification()){
-            String title = title_etNE.getText().toString();
+        // אם הנתונים תקניים שמירה והעלאה של הקובץ
+        if (dataVerification(title,note_txt)){
             note.setTitle(title);
-            String dateTime_created = getDateAndTime();
+            dateTime_created = getDateAndTime();
             note.setDateTime_created(dateTime_created);
             String txt = getTxtPath();
-            createTxtFile(txt);
+            createTxtFile(note_txt, txt);
+            note.setTxt(txt);
+            notesRef.child(currentUser.getUid()).child(thumbtackStatus).child(dateTime_created).setValue(note);
+
+            //סיום ויצאה מהActivity
+            Intent newActivity;
+            newActivity = new Intent(NotesEdit.this, NotesView.class);
+            startActivity(newActivity);
         }
-
-
-
-
-        Intent newActivity;
-        newActivity = new Intent(NotesEdit.this, NotesView.class);
-        startActivity(newActivity);
     }
 
-    private void createTxtFile(String txtPath) {
-        String note_txt = txt_etNE.getText().toString();
+    //יצירה וההעלאה של קובץ Txt לFBST
+    private void createTxtFile(String note_txt, String txtPath) {
         byte[] note_byte = note_txt.getBytes();
         storageRef.child(txtPath).putBytes(note_byte);
     }
 
+    // יצירת כתובת לאחסון הנתונים
     private String getTxtPath() {
-        if (thumbtack_cbNE.isChecked())
-            return ("Notes/" + currentUser.getUid() + "/thumbtack" + ".txt");
+        if (thumbtack_cbNE.isChecked()) {
+            thumbtackStatus = "Thumbtack";
+            return ("Notes/" + currentUser.getUid() + "/Thumbtack/" + dateTime_created + ".txt");
+        }
         else
-            return ("Notes/" + currentUser.getUid() + "/thumbtackNo" + ".txt");
+            return ("Notes/" + currentUser.getUid() + "/noThumbtack/" + dateTime_created + ".txt");
     }
 
-    private boolean dataVerification() {
+    //בדיקה האם הפרטים שהוכנסו נכונים
+    private boolean dataVerification(String title, String txt) {
+        int errorExist = 0;
+        if (title.length()<1){
+            title_etNE.setError("ERROR! The filed can't be blank.");
+            errorExist++;
+        }
+        if (txt.length()<1){
+            txt_etNE.setError("ERROR! Check the email.");
+            errorExist++;
+        }
+        if(errorExist > 0){
+            return false;
+        }
         return true;
     }
 
+    //המרת תאריך ושעה לפורמט רצוי
     private String getDateAndTime() {
-        SimpleDateFormat dateFormat = new SimpleDateFormat("YYYYMMDDhhmmss");
-        Date date = new Date();
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyyMMddHHmmss");
+        Date date = Calendar.getInstance().getTime();
         return dateFormat.format(date);
     }
 }
