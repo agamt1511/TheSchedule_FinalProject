25baba353ab379519c310d8b5d5a705f53c5ece5
agamt1511 <at1511@bs.amalnet.k12.il>
2023-05-30 16:43:38 +0300

    java doc.

--- app/src/main/java/com/example/theschedule_finalproject/Credits.java
+++ app/src/main/java/com/example/theschedule_finalproject/Credits.java
@@ -16,6 +16,9 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
+/**
+ * @author Agam Toledano
+ */
 public class Credits extends AppCompatActivity {
     Intent newActivity;
     TextView showCredits;
